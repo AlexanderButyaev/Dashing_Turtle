fd342973468dcf6369d5bde2b7b06ad7db213649
J. White Bear <16785523+jwbear@users.noreply.github.com>
2025-07-31 18:55:05 -0400

    update dependencies

--- conda-recipe/build.bat
+++ conda-recipe/build.bat
@@ -3,10 +3,7 @@ setlocal enabledelayedexpansion
 set PY_VER=%PY_MAJOR%.%PY_MINOR%
 
 :: Install from local wheels
-@REM %PYTHON% -m pip install --no-index --find-links=%SRC_DIR%\conda-recipe\wheels svgpathtools==1.6.1
 %PYTHON% -m pip install --no-index --find-links=%SRC_DIR%\conda-recipe\wheels varnaapi
-@REM %PYTHON% -m pip install --no-index --find-links=%SRC_DIR%\conda-recipe\wheels matplotlib==3.9.0
-@REM %PYTHON% -m pip install --no-index --find-links=%SRC_DIR%\conda-recipe\wheels tzdata
 @REM %PYTHON% -m pip install --no-index --find-links=%SRC_DIR%\conda-recipe\wheels PyQt6 PyQt6-sip PyQt6-Qt6
 
 $PYTHON% -m pip install mariadb
