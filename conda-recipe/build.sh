6bc1e77f91d3b6ca75d137d321a4e80034a9527e
J. White Bear <16785523+jwbear@users.noreply.github.com>
2025-07-31 18:35:32 -0400

    update dependencies

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -6,12 +6,12 @@ set -euxo pipefail
 $PYTHON -m pip install --no-index --find-links=$SRC_DIR/conda-recipe/wheels varnaapi
 #$PYTHON -m pip install --no-index --find-links=$SRC_DIR/conda-recipe/wheels matplotlib==3.9.0
 #$PYTHON -m pip install --no-index --find-links=$SRC_DIR/conda-recipe/wheels tzdata
-#if [[ "$(uname)" == "Darwin" && "$(uname -m)" == "arm64" ]]; then
+if [[ "$(uname)" == "Darwin" && "$(uname -m)" == "arm64" ]]; then
 #  $PYTHON -m pip install --no-index --find-links=$SRC_DIR/conda-recipe/wheels PyQt6 PyQt6-sip PyQt6-Qt6
-#  $PYTHON -m pip install mariadb
-#  $PYTHON -m pip install snowflake-id
-#  $PYTHON -m pip install pysam
-#fi
+  $PYTHON -m pip install mariadb
+  $PYTHON -m pip install snowflake-id
+  #$PYTHON -m pip install pysam
+fi
 
 # Install the package using pip
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
