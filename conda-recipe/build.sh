614af07efd7f245e0c683b5efd47f3bb4f2ea74e
J. White Bear <16785523+jwbear@users.noreply.github.com>
2025-08-01 01:05:22 -0400

    update dependencies

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -5,12 +5,12 @@ set -euxo pipefail
 $PYTHON -m pip install --no-index --find-links=$SRC_DIR/conda-recipe/wheels varnaapi
 #$PYTHON -m pip install mariadb --no-index --find-links=$SRC_DIR/conda-recipe/wheels
 
-if [[ "$(uname)" == "Darwin" && "$(uname -m)" == "arm64" ]]; then
+#if [[ "$(uname)" == "Darwin" && "$(uname -m)" == "arm64" ]]; then
   #$PYTHON -m pip install --no-index --find-links=$SRC_DIR/conda-recipe/wheels PyQt6 PyQt6-sip PyQt6-Qt6
   #$PYTHON -m pip install $SRC_DIR/conda-recipe/wheels/mariadb-1.1.13.tar.gz
   #$PYTHON -m pip install snowflake-id --no-index --find-links=$SRC_DIR/conda-recipe/wheels
   #$PYTHON -m pip install pysam --no-index --find-links=$SRC_DIR/conda-recipe/wheels/pysam-0.23.3-cp39-cp39-macosx_11_0_arm64.whl
-fi
+#fi
 
 # Install the package using pip
 $PYTHON -m pip install . --no-deps --ignore-installed -vv
