178e570a6eb1f61738352e575b6a61e6d4c79031
J. White Bear <16785523+jwbear@users.noreply.github.com>
2025-07-31 21:40:16 -0400

    update dependencies

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -9,7 +9,7 @@ if [[ "$(uname)" == "Darwin" && "$(uname -m)" == "arm64" ]]; then
   #$PYTHON -m pip install mariadb --verbose
   #$PYTHON -m pip install snowflake-id
   #$PYTHON -m pip install PyQt6 PyQt6-sip PyQt6-Qt6
-  $PYTHON -m pip install pysam --verbose
+  $PYTHON -m pip install pysam --index-url https://pypi.org/simple
 fi
 
 # Install the package using pip
