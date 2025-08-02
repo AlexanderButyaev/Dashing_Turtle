7d7df40e404051c55498fa99cd2f0db753f27a95
J. White Bear <16785523+jwbear@users.noreply.github.com>
2025-08-02 18:08:22 -0400

    update dependencies

--- conda-recipe/build.bat
+++ conda-recipe/build.bat
@@ -4,17 +4,19 @@ set PY_VER=%PY_MAJOR%.%PY_MINOR%
 
 :: Install from local wheels
 %PYTHON% -m pip install --no-index --find-links=%SRC_DIR%\conda-recipe\wheels varnaapi
+%PYTHON% -m pip install --no-index --find-links=%SRC_DIR%\conda-recipe\wheels cython
 %PYTHON% -m pip install %SRC_DIR%\conda-recipe\wheels\pysam-0.23.3.tar.gz
-%PYTHON% -m pip --no-index --find-links=%SRC_DIR%\conda-recipe\wheels pyqt6
-%PYTHON% -m pip --no-index --find-links=%SRC_DIR%\conda-recipe\wheels pyqt6-sip
-%PYTHON% -m pip --no-index --find-links=%SRC_DIR%\conda-recipe\wheels pyqt6-qt6
+%PYTHON% -m pip install --no-index --find-links=%SRC_DIR%\conda-recipe\wheels pyqt6
+%PYTHON% -m pip install --no-index --find-links=%SRC_DIR%\conda-recipe\wheels pyqt6-sip
+%PYTHON% -m pip install --no-index --find-links=%SRC_DIR%\conda-recipe\wheels pyqt6-qt6
 
 :: Install the package
 %PYTHON% -m pip install . --no-deps --ignore-installed -vv
 
 :: Rename the original script
-rename "%PREFIX%\Scripts\dt-gui.exe" .dt-gui-real.exe
-
+if exist "%PREFIX%\Scripts\dt-gui.exe" (
+    rename "%PREFIX%\Scripts\dt-gui.exe" .dt-gui-real.exe
+)
 :: Create a launcher script wrapper (dt-gui.bat)
 (
 echo @echo off
