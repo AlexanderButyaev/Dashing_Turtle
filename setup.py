eb1df807687af6b3f6c66efc12008f738c1b1bc1
J. White Bear <16785523+jwbear@users.noreply.github.com>
2025-08-02 21:42:07 -0400

    anaconda push

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ def parse_requirements(filename):
 
 setup(
     name="DashingTurtle",
-    version="0.1.6",
     author="J. White Bear",
     author_email="jwbear15@gmail.com",
     description="An applicaton for building structural landscapes of RNA sequence modifications with Nanopore data.",
@@ -19,6 +18,8 @@ setup(
     packages=find_packages(),
     install_requires=parse_requirements("requirements.txt"),
     include_package_data=True,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     package_data={
         "DashML.Varna": ["VARNAv3-93.jar"],
     },
